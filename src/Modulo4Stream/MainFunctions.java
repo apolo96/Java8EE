88f3206f620385cdd7cd6c1461338dd8b257445e
Andres Felipe <andresfpvclap@gmail.com>
2016-12-06 15:50:07 -0500

    Stream y functional interface

--- src/Modulo4Stream/MainFunctions.java
+++ src/Modulo4Stream/MainFunctions.java
@@ -17,10 +17,11 @@ public class MainFunctions {
     public static void main(String[] args) {
         List<Persona> mundo = Persona.crearMundo();
         OperacionesImplementadas operaciones = new OperacionesImplementadas();
+        System.out.println("Todas las personas");
         operaciones.mostrarPersonas(mundo);
-        System.out.println('M');
+        System.out.println("Personas del genero Masculino");
         System.out.println(operaciones.filtroPersonas(mundo,operaciones.esMasculino()));
-        System.out.println('F');
+        System.out.println("Peronsas del genero Femenino");
         operaciones.mostrarFiltroPersonas(mundo,operaciones.esFemenino());
         System.out.println("Persona de 25 años de edad y sexo masculino");
         operaciones.mostrarFiltroPersonas(mundo,operaciones.cumpleCondiciones(25, 'M'));
